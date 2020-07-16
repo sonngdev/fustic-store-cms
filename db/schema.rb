07b6c2c427d928df41cc2df66cdfcd193ced5d4b
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-07-16 23:45:29 +0700

    feat: generate category scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_015103) do
+ActiveRecord::Schema.define(version: 2020_07_16_164447) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
