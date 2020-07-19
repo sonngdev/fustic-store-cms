7d1548a9355df41c50409b192cbaccd9bffdb0d5
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-07-19 14:56:13 +0700

    feat: destroy image manager along with product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
 
   belongs_to :category
 
-  has_one :product_image_manager
+  has_one :product_image_manager, dependent: :destroy
 
   has_many_attached :images
 
