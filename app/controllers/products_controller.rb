3101fc93ca3fe08adf44240e6e680f0e7dfdec69
Nguyễn Thanh Sơn <son@gotitapp.co>
2020-07-19 13:06:51 +0700

    feat: allow creating product with images

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -74,6 +74,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :slug, :price_vnd, :price_usd, :category_id)
+      params.require(:product).permit(:name, :slug, :price_vnd, :price_usd, :category_id, images: [])
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   belongs_to :category
 
   has_one :product_image_manager
+
+  has_many_attached :images
 end
